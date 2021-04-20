e6f34dff7a920d6f9a876a9a8f26797bb935ad2e
Marta Kanska <kanska.marta@outlook.com>
2021-04-20 17:57:47 +0200

    add cucumber

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,12 +11,22 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const cucumber = require("cypress-cucumber-preprocessor").default;
+const browserify = require("@cypress/browserify-preprocessor");
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+  const options = browserify.defaultOptions;
+
+  options.browserifyOptions.plugin.unshift([
+    "tsify",
+    { project: "cypress/tsconfig.json" },
+  ]);
+
+  on("file:preprocessor", cucumber());
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
